9ac02f2c1e8c7bc3471b216c58cc7d00b4a17765
Scott Borecki <scottborecki@gmail.com>
2021-08-24 14:49:29 -0600

    Create welcome index page
    Co-Authored-By: AntonioK <antoniojking@users.noreply.github.com>

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -10,20 +10,8 @@ require 'rails_helper'
 # Details: Implement basic auth in the application allowing a user to log in with an email and password. The password should be stored in the database using bcrypt.
 
 RSpec.describe "User registration form" do
-  it "creates new user" do
+  xit "creates new user" do
     visit root_path
-
-    welcome_message = 'Welcome to Viewing Party!'
-    description = 'This is the Viewing Party.  Create an event, invite your friends, watch a movie together, have a great time, and repeat!'
-    button_text = 'Log In'
-    registration_link = 'Register as a User'
-
-    expect(page).to have_current_path(root_path)
-
-    expect(page).to have_content(welcome_message)
-    expect(page).to have_content(description)
-    expect(page).to have_button(button_text)
-    expect(page).to have_link(registration_link)
 
     click_on registration_link
 
