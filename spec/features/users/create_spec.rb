f3acfa02250faa5203522774786c7e6232c2559c
Antonio King <antonio.j.king@gmail.com>
2021-08-26 13:10:41 -0600

    Add invalid registration test and flash messages
    Co-Authored-By: Scott Borecki <Scott-Borecki@users.noreply.github.com>

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -10,7 +10,7 @@ require 'rails_helper'
 # Details: Implement basic auth in the application allowing a user to log in with an email and password. The password should be stored in the database using bcrypt.
 
 RSpec.describe 'User registration form' do
-  it 'creates new user' do
+  it 'creates new user, happy path' do
     visit root_path
 
     registration_link = 'Register as a User'
@@ -28,8 +28,37 @@ RSpec.describe 'User registration form' do
     fill_in 'user[password_confirmation]', with: password
 
     click_on submit
-    
+
     expect(page).to have_current_path('/dashboard')
-    expect(page).to have_content("Welcome, #{email}!")
+
+    within '#success' do
+      expect(page).to have_content("Welcome, #{email}!")
+    end
+  end
+
+  it 'creates new user, sad path' do
+    visit root_path
+
+    registration_link = 'Register as a User'
+
+    click_on registration_link
+
+    expect(page).to have_current_path('/registration')
+
+    email = 'funbucket13'
+    password = 'test'
+    submit = 'Register'
+
+    # fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on submit
+
+    expect(page).to have_current_path('/registration')
+
+    within '#failure' do
+      expect(page).to have_content('Something went horribly wrong!')
+    end
   end
 end
