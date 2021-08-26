21409927cef79e42ec355bfc502e3e910e2493da
Antonio King <antonio.j.king@gmail.com>
2021-08-26 13:19:17 -0600

    Add invalid registration tests for passwords
    Co-Authored-By: Scott Borecki <Scott-Borecki@users.noreply.github.com>

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'User registration form' do
     end
   end
 
-  it 'creates new user, sad path' do
+  it 'creates new user, sad path: no email' do
     visit root_path
 
     registration_link = 'Register as a User'
@@ -49,9 +49,60 @@ RSpec.describe 'User registration form' do
     password = 'test'
     submit = 'Register'
 
-    # fill_in 'user[email]', with: email
     fill_in 'user[password]', with: password
     fill_in 'user[password_confirmation]', with: password
+
+    click_on submit
+
+    expect(page).to have_current_path('/registration')
+
+    within '#failure' do
+      expect(page).to have_content('Something went horribly wrong!')
+    end
+  end
+
+  it 'creates new user, sad path: no password' do
+    visit root_path
+
+    registration_link = 'Register as a User'
+
+    click_on registration_link
+
+    expect(page).to have_current_path('/registration')
+
+    email = 'funbucket13'
+    password = 'test'
+    submit = 'Register'
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on submit
+
+    expect(page).to have_current_path('/registration')
+
+    within '#failure' do
+      expect(page).to have_content('Something went horribly wrong!')
+    end
+  end
+
+  it 'creates new user, sad path: inconsistent password' do
+    visit root_path
+
+    registration_link = 'Register as a User'
+
+    click_on registration_link
+
+    expect(page).to have_current_path('/registration')
+
+    email = 'funbucket13'
+    password = 'test'
+    password2 = 'tset'
+    submit = 'Register'
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password2
 
     click_on submit
 
