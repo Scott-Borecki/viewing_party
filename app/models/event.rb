3217775095c6f395a7e9a29582d77c43bf249b4d
Scott Borecki <scottborecki@gmail.com>
2021-08-31 14:09:15 -0600

    Add validation so duration can't be less than movie runtime
    Co-Authored-By: AntonioK <antoniojking@users.noreply.github.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,8 +7,16 @@ class Event < ApplicationRecord
   validates :movie_id, presence: true, numericality: true
   validates :date_time, presence: true
   validates :duration, presence: true, numericality: true
+  validate :duration_cannot_be_less_than_movie_runtime
 
   def movie_title
     MovieFacade.search_by_id(movie_id).title
+  end
+
+  def duration_cannot_be_less_than_movie_runtime
+    return unless movie_id.present? && movie_id > 0
+    if duration < MovieFacade.search_by_id(movie_id).runtime
+      errors.add(:duration, "can't be less than the movie runtime")
+    end
   end
 end
