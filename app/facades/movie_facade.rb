50f99e37a3e73776a0f00bde475ab9fd49391be5
Antonio King <antonio.j.king@gmail.com>
2021-08-30 19:17:00 -0600

    Add movie_data, cast_members, and reviews methods to facade
    Co-Authored-By: Scott Borecki <Scott-Borecki@users.noreply.github.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,9 +1,31 @@
 class MovieFacade
-  def self.search_by_title(search_by_title)
-    json = MovieService.search_by_title(search_by_title)
+  def self.search_by_title(title)
+    json = MovieService.search_by_title(title)
 
     json[:results].map do |movie_data|
       Movie.new(movie_data)
+    end
+  end
+
+  def self.search_by_id(movie_id)
+    json = MovieService.search_by_id(movie_id)
+
+    Movie.new(json)
+  end
+
+  def self.cast_members_by_movie_id(movie_id)
+    json = MovieService.cast_members_by_movie_id(movie_id)
+
+    json[:cast].map do |actor_data|
+      Actor.new(actor_data)
+    end
+  end
+
+  def self.reviews_by_movie_id(movie_id)
+    json = MovieService.reviews_by_movie_id(movie_id)
+
+    json[:results].map do |review_data|
+      Review.new(review_data)
     end
   end
 end
