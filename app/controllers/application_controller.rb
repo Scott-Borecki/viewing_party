d1a1a94eabd6384a1b9f5ad07bfcf8683d852c43
Scott Borecki <scottborecki@gmail.com>
2021-08-31 14:08:03 -0600

    Add #error_message helper method
    Co-Authored-By: AntonioK <antoniojking@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
+
+  private
+
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
 end
