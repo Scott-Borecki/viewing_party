3b9d055a733d5553f73d8468c7b07e8af35d53df
Scott Borecki <scottborecki@gmail.com>
2021-08-31 17:51:41 -0600

    Add trending movies button

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -35,6 +35,27 @@ RSpec.describe 'movie discover page' do
         end
       end
 
+      it 'displays a button to display the trending movies of the week' do
+        expect(page).to have_button('Find Trending Movies')
+      end
+
+      context 'when I click on the "Find Trending Movies" button', :vcr do
+        before { click_button('Find Trending Movies') }
+
+        it 'redirects me to the movies page' do
+          expect(current_path).to eq('/movies')
+        end
+
+        it 'displays the trending movies' do
+          trending_movies = MovieFacade.trending_movies
+
+          trending_movies.each do |movie|
+            expect(page).to have_content(movie.title)
+            expect(page).to have_content(movie.vote_average)
+          end
+        end
+      end
+
       it 'displays a form to search by movie title' do
         expect(page).to have_field(:search)
         expect(page).to have_button('Find Movies')
